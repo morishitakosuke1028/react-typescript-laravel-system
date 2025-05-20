496464716d1590f3c0707b97021bca309c8b912d
morishitakosuke1028 <k.morishita1028@outlook.jp>
2025-05-20 11:25:35 +0900

    feat: レンタカーモデル登録実装

--- app/Models/RentalCar.php
+++ app/Models/RentalCar.php
@@ -18,4 +18,25 @@ class RentalCar extends Model
         'car_image_rear',
         'memo',
     ];
+
+    public static function createRentalCar(array $attributes)
+    {
+        if (isset($attributes['new_car_image_front']) && $attributes['new_car_image_front'] instanceof \Illuminate\Http\UploadedFile) {
+            $attributes['car_image_front'] = $attributes['new_car_image_front']->store('car_image_front', 'public');
+        } else {
+            unset($attributes['car_image_front']);
+        }
+        if (isset($attributes['new_car_image_side']) && $attributes['new_car_image_side'] instanceof \Illuminate\Http\UploadedFile) {
+            $attributes['car_image_side'] = $attributes['new_car_image_side']->store('car_image_side', 'public');
+        } else {
+            unset($attributes['car_image_side']);
+        }
+        if (isset($attributes['new_car_image_rear']) && $attributes['new_car_image_rear'] instanceof \Illuminate\Http\UploadedFile) {
+            $attributes['car_image_rear'] = $attributes['new_car_image_rear']->store('car_image_rear', 'public');
+        } else {
+            unset($attributes['car_image_rear']);
+        }
+
+        return self::create($attributes);
+    }
 }
