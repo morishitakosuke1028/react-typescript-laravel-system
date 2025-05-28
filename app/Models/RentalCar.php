ab30f2011486895b0002ae19c37cedb7287d9424
morishitakosuke1028 <k.morishita1028@outlook.jp>
2025-05-28 11:52:18 +0900

    feat: レンタカー登録処理修正

--- app/Models/RentalCar.php
+++ app/Models/RentalCar.php
@@ -19,22 +19,28 @@ class RentalCar extends Model
         'memo',
     ];
 
-    public static function createRentalCar(array $attributes)
+    public static function createRentalCar(array $attributes): self
     {
-        if (isset($attributes['new_car_image_front']) && $attributes['new_car_image_front'] instanceof \Illuminate\Http\UploadedFile) {
-            $attributes['car_image_front'] = $attributes['new_car_image_front']->store('car_image_front', 'public');
-        } else {
-            unset($attributes['car_image_front']);
-        }
-        if (isset($attributes['new_car_image_side']) && $attributes['new_car_image_side'] instanceof \Illuminate\Http\UploadedFile) {
-            $attributes['car_image_side'] = $attributes['new_car_image_side']->store('car_image_side', 'public');
-        } else {
-            unset($attributes['car_image_side']);
-        }
-        if (isset($attributes['new_car_image_rear']) && $attributes['new_car_image_rear'] instanceof \Illuminate\Http\UploadedFile) {
-            $attributes['car_image_rear'] = $attributes['new_car_image_rear']->store('car_image_rear', 'public');
-        } else {
-            unset($attributes['car_image_rear']);
+        foreach ([
+            'car_image_front',
+            'car_image_side',
+            'car_image_rear',
+        ] as $field) {
+            $newKey = 'new_' . $field;
+
+            if (isset($attributes[$newKey]) && $attributes[$newKey] instanceof \Illuminate\Http\UploadedFile) {
+                $attributes[$field] = $attributes[$newKey]->store($field, 'public');
+            } elseif (!empty($attributes[$field]) && is_string($attributes[$field])) {
+                $tempPath = $attributes[$field];
+                $filename = basename($tempPath);
+                $newPath = "rental_car_images/{$filename}";
+
+                if (Storage::disk('public')->exists("temp_rental_car_images/{$filename}")) {
+                    Storage::disk('public')->move("temp_rental_car_images/{$filename}", $newPath);
+                    $attributes[$field] = $newPath;
+                }
+            }
+            unset($attributes[$newKey]);
         }
 
         return self::create($attributes);
