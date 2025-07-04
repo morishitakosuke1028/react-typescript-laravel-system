c2affa6ac501dde555bf7ae9fa3be1ed4f5774ba
morishitakosuke1028 <k.morishita1028@outlook.jp>
2025-07-04 15:54:35 +0900

    feat: テスト

--- database/factories/MInsuranceCompanyFactory.php
+++ database/factories/MInsuranceCompanyFactory.php
@@ -17,7 +17,12 @@ class MInsuranceCompanyFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'insurance_company_name' => $this->faker->company,
+            'insurance_company_kana' => $this->faker->kanaName,
+            'policy_number' => $this->faker->bothify('POL#######'),
+            'person_name' => $this->faker->name,
+            'tel' => $this->faker->phoneNumber,
+            'email' => $this->faker->safeEmail,
         ];
     }
 }
