e70b683dcaa3b649429b49d53c380d645058d1ef
morishitakosuke1028 <k.morishita1028@outlook.jp>
2025-07-28 09:18:37 +0900

    feat: 単価マスタテスト

--- tests/Feature/MUnitPriceControllerTest.php
+++ tests/Feature/MUnitPriceControllerTest.php
@@ -2,19 +2,30 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use App\Models\MUnitPrice;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
 class MUnitPriceControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
+    use WithoutMiddleware;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->actingAs(User::factory()->create());
+        $this->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
+    }
+
+    public function test_index_displays_unit_prices()
+    {
+        MUnitPrice::factory()->count(3)->create();
+
+        $response = $this->get(route('MUnitPrices.index'));
         $response->assertStatus(200);
     }
 }
